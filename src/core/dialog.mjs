936bc983ea90a97079a8f6f818bb921b71051d5e
Morisson Marcel <115091 04+morissonmaciel@users.noreply.github.com>
2026-02-07 10:58:47 -0300

    Fix button2 hover colors

--- src/core/dialog.mjs
+++ src/core/dialog.mjs
@@ -1,10 +1,7 @@
 import Bunnix, {
   useRef,
   useState,
-  useEffect,
-  useMemo,
   Show,
-  ForEach,
 } from "@bunnix/core";
 import { Heading } from "./typography.mjs";
 import { Column, Row, Spacer } from "./layout.mjs";
@@ -23,12 +20,6 @@ export const useDialog = () => {
 
   return {
     Dialog: () => {
-      const title = dialogState.map((s) => s.title);
-      const contents = dialogState.map((s) => s.contents);
-      const confirmationText = dialogState.map((s) => s.confirmation.text);
-      const confirmationVariant = dialogState.map((s) => s.confirmation.variant);
-      const confirmationAction = dialogState.map((s) => s.confirmation.action);
-
       const closeDialog = () => {
         dialogRef.current?.close();
       };
@@ -37,48 +28,55 @@ export const useDialog = () => {
         {
           ref: dialogRef,
           cancel: closeDialog,
-          class: `border-primary radius-lg backdrop-dimmed`,
+          class: `border-primary radius-lg backdrop-dimmed padding-sm`,
         },
-        Column(
-          { class: "padding-md", gap: 12, maxWidth: 480 },
-          Row(
-            { gap: 24, alignItems: "start" },
-            Heading({ h3: true, flexShrink: 0 }, title),
-            Spacer(),
-            Button2(
-              { variant: "tertiary", click: closeDialog },
-              Icon2({ name: "close", color: "secondary", size: 20 }),
-            ),
-          ),
-          Show(
-            contents.map((c) => c.length > 0),
-            () => Column(...contents.get()),
-          ),
-          Row(
-            Spacer(),
-            Button2(
-              {
-                minWidth: 80,
-                variant: confirmationVariant.get(),
-                click: () => {
-                  let action = confirmationAction.get();
-                  (action) && action();
-                  closeDialog();
-                }
-              }, confirmationText)
-          ),
-        ),
+        Show(
+          dialogState,
+          (state) => (
+            Column(
+              { class: "padding-md", gap: 12, minWidth: 420, maxWidth: 580 },
+              Row(
+                { gap: 24, alignItems: "center" },
+                Heading({ h3: true, flexShrink: 0 }, state.title),
+                Spacer(),
+                Button2(
+                  { variant: "tertiary", click: closeDialog },
+                  Icon2({ name: "close", color: "secondary", size: 20 }),
+                ),
+              ),
+              state.contents.length > 0 && Column(...state.contents),
+              Row(
+                Spacer(),
+                Button2(
+                  {
+                    minWidth: 80,
+                    variant: state.confirmation.variant,
+                    click: () => {
+                      let action = state.confirmation.action;
+                      (action) && action();
+                      closeDialog();
+                    }
+                  },
+                  state.confirmation.text
+                )
+              ),
+            )
+          )
+        )
       );
     },
     showDialog: ({
       title,
       contents = [],
-      confirmation = { text: "OK", variant: "primary", action },
+      confirmation,
     } = {}) => {
+      const defaultConfirmation = { text: "OK", variant: "primary", action: null };
+      const mergedConfirmation = { ...defaultConfirmation, ...confirmation };
+
       dialogState.set({
         title,
-        contents: typeof contents === "array" ? contents : [contents],
-        confirmation,
+        contents: Array.isArray(contents) ? contents : [contents],
+        confirmation: mergedConfirmation,
       });
       dialogRef.current?.showModal();
     },
